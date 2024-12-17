8814c451ddf381874aed03f886513d0d64a71db6
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-17 08:22:40 -0500

    refactor: new schema for users, registration route validates new inputs

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -4,6 +4,9 @@ const { genPassword, issueJWT } = require('../utils/passwordUtils');
 
 const prisma = new PrismaClient();
 
+const existsErr = 'field is required!';
+const usernameLengthErr = 'must be between 5 and 30 characters.';
+const usernameAlphaNumErr = 'must only contain letters and numbers.';
 const nameAlphaErr = 'must only contain letters.';
 const nameLengthErr = 'must be between 1 and 25 characters.';
 const emailErr = 'Must be a valid email address.';
@@ -11,6 +14,15 @@ const passLengthErr = 'must be between 8 and 32 characters.';
 const passMatchErr = 'Passwords must match.';
 
 const validateUser = [
+  body('username')
+    .exists({ values: 'undefined' | 'null' })
+    .withMessage(`Username ${existsErr}`)
+    .trim()
+    .isLength({ min: 5, max: 30 })
+    .withMessage(`Username ${usernameLengthErr}`)
+    .isAlphanumeric()
+    .withMessage(`Username ${usernameAlphaNumErr}`),
+  body('email').trim().isEmail().withMessage(emailErr),
   body('firstname')
     .trim()
     .isAlpha()
@@ -23,12 +35,16 @@ const validateUser = [
     .withMessage(`Last name ${nameAlphaErr}`)
     .isLength({ min: 1, max: 25 })
     .withMessage(`Last name ${nameLengthErr}`),
-  body('email').trim().isEmail().withMessage(emailErr),
   body('password')
+    .exists({ values: 'undefined' | 'null' })
+    .withMessage(`Password ${existsErr}`)
     .trim()
     .isLength({ min: 8, max: 32 })
     .withMessage(`Password ${passLengthErr}`),
   body('confirmPassword')
+    .exists({ values: 'undefined' | 'null' })
+    .withMessage(`Confirm password ${existsErr}`)
+    .trim()
     .custom((value, { req }) => value === req.body.password)
     .withMessage(passMatchErr),
 ];
@@ -45,9 +61,10 @@ const postNewUser = [
     try {
       const user = await prisma.user.create({
         data: {
+          username: req.body.username,
+          email: req.body.email,
           firstName: req.body.firstname,
           lastName: req.body.lastname,
-          email: req.body.email,
           hash,
           salt,
         },
