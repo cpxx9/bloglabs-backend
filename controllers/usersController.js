bd5c8a59ea15cafbd23bf28e4accbf9c348f8627
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-18 06:18:45 -0500

    fix: wrap query in try catch

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,20 +2,24 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const listUsers = async (req, res) => {
-  const users = await prisma.user.findMany({
-    select: {
-      created: true,
-      updated: true,
-      username: true,
-      email: true,
-      firstname: true,
-      lastname: true,
-      posts: true,
-      comments: true,
-    },
-  });
-  res.status(200).json({ success: true, data: users });
+const listUsers = async (req, res, next) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        created: true,
+        updated: true,
+        username: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        posts: true,
+        comments: true,
+      },
+    });
+    res.status(200).json({ success: true, data: users });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const listUser = async (req, res, next) => {
