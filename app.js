3f008035bc2d797c9e479fab52edaadb519f8812
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-12-26 21:31:01 -0500

    fix: cors options

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const allowlist = ['http://localhost:3000', 'http://blog.cjplabs.com'];
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowlist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
 
-app.use(cors(corsOptionsDelegate));
+app.use(cors(corsOptions));
 
 app.use('/api', indexRouter);
 app.use('*', notFound);
