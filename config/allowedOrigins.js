23e6295af1c9fc82da816a6abc704d752a467061
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-01-31 07:22:28 -0500

    feat: allow from another port

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 module.exports.allowedOrigins = [
   'http://localhost:3000',
+  'http://localhost:3001',
   'http://blog.cjplabs.com',
   'http://127.0.0.1:3000',
-  undefined,
+  'http://127.0.0.1:3001',
 ];
