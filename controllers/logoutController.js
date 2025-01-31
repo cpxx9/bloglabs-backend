333fecca9db61fb65f1b46dca1712df0c7d94d71
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-01-30 20:13:46 -0500

    fix: logout finds correct user

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -6,13 +6,15 @@ const logoutController = async (req, res, next) => {
   // On frontend, delete the access token from memory
   const { cookies } = req;
   if (!cookies?.jwt) return res.sendStatus(204);
-  const refreshToken = cookies.jwt.split(' ')[1];
+  const refreshToken = cookies.jwt;
+  console.log('refresh', refreshToken);
   try {
     const user = await prisma.user.findFirst({
       where: {
         refresh: refreshToken,
       },
     });
+    console.log(user);
     if (!user) {
       res.clearCookie('jwt', { httpOnly: true });
       return res.sendStatus(204);
@@ -26,6 +28,7 @@ const logoutController = async (req, res, next) => {
       },
     });
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // add secure: true - https
+    console.log(res);
     res.sendStatus(204);
   } catch (err) {
     return next(err);
