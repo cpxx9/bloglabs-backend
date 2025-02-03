47c31310c0c0925364e93aa658a84c6a4673e6a8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 20:29:10 -0500

    feat: cors options

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,9 @@ module.exports.allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://blog.cjplabs.com',
+  'https://blog.cjplabs.com',
+  'https://bloglabs-editor.cjplabs.com',
+  'http://bloglabs-editor.cjplabs.com',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
 ];
