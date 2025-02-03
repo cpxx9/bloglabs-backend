c0e76da74d09ae93b7e017e7f09ba92b857592eb
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-02-02 21:11:35 -0500

    test

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -11,7 +11,7 @@ const postNewUser = [
   async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors);
+      console.log('1', errors);
       return res.status(400).json({ success: false, errors: errors.array() });
     }
 
