111e78dfbbc420eb613ab5184fbdcde798cd69e8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2025-11-06 07:58:05 -0500

    fix: allowed origins include login pages

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,9 +3,13 @@ module.exports.allowedOrigins = [
   'https://localhost:3000',
   'http://localhost:3001',
   'http://blog.cjplabs.com',
+  'http://blog.cjplabs.com/login',
   'https://blog.cjplabs.com',
+  'https://blog.cjplabs.com/login',
   'https://bloglabs-editor.cjplabs.com',
+  'https://bloglabs-editor.cjplabs.com/login',
   'http://bloglabs-editor.cjplabs.com',
+  'http://bloglabs-editor.cjplabs.com/login',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
 ];
